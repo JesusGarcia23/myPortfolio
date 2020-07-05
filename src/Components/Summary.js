74cdbef09e1c3356ced663a16334aaa1588767b1
= <jacex17@gmail.com>
2020-07-05 16:22:56 -0700

    add more text to summary page

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -11,8 +11,11 @@ const Summary = (props) => {
         <div className= 'summary-text-container'>
         <div className='summary-text'>
         I'm a passionate full-stack web developer who loves technology in general,
-         
+        new challenges, and learning every day. I enjoy building software applications,
+        solving problems, working in teams, making ideas happen.
         </div>
+        <p>"The function of good software is to make the complex appear to be simple."</p>
+        <i>- Grady Booch</i>
         </div>
 
         </div>
